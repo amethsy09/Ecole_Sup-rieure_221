2258124bdc89945856f4fd39cf3e8b7b9fba3371
Fatou badara SALL <167468174+faasaliou123@users.noreply.github.com>
2026-03-01 16:04:08 +0000

    Refactor classe routes to use class-based structure

--- src/routes/classe.routes.js
+++ src/routes/classe.routes.js
@@ -1,25 +1,23 @@
-import { Router } from "express";
+import express from "express";
 import ClasseController from "../controllers/classe.controller.js";
 
-const router = Router();
-const classeController = new ClasseController();
+export default class ClasseRoute {
+  constructor() {
+    this.router = express.Router();
+    this.controller = new ClasseController();
+    this.initializeRoutes();
+  }
 
-// POST /api/classes - Créer une classe
-router.post("/", (req, res) => classeController.createClasse(req, res));
+  initializeRoutes() {
+    this.router.get("/", this.controller.getAllClasses.bind(this.controller));
+    this.router.get("/:id", this.controller.getClasseById.bind(this.controller));
+    this.router.post("/", this.controller.createClasse.bind(this.controller));
+    this.router.put("/:id", this.controller.updateClasse.bind(this.controller));
+    this.router.patch("/:id/archive", this.controller.archiveClasse.bind(this.controller));
+    this.router.delete("/:id", this.controller.deleteClasse.bind(this.controller));
+  }
 
-// GET /api/classes - Lister les classes (?archived=true pour inclure archivées)
-router.get("/", (req, res) => classeController.getAllClasses(req, res));
-
-// GET /api/classes/:id - Détail d'une classe
-router.get("/:id", (req, res) => classeController.getClasseById(req, res));
-
-// PUT /api/classes/:id - Modifier une classe
-router.put("/:id", (req, res) => classeController.updateClasse(req, res));
-
-// PATCH /api/classes/:id/archive - Archiver une classe
-router.patch("/:id/archive", (req, res) => classeController.archiveClasse(req, res));
-
-// DELETE /api/classes/:id - Supprimer une classe
-router.delete("/:id", (req, res) => classeController.deleteClasse(req, res));
-
-export default router;
+  getRouter() {
+    return this.router;
+  }
+}
